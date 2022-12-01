9e581debca86adb1514f43021da056e7c7ff355b
luiscardenasfs <luiscardenasfs@gmail.com>
2022-11-30 19:01:01 -0800

    bugsFixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+// Components
+import Item from './item/item';
+import Drawer from '@mui/material'
+import { LinearProgress } from '@mui/material';
+import { Grid } from '@mui/material';
+import { AddShoppingCart } from '@mui/icons-material';
+import {Badge} from '@mui/material'
+// Styles
+import { Wrapper } from './App.styles'
+//types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
+
+const App= () => {
+  const { data, isLoading, error} = useQuery<CartItemType[]>(
+    'products',
+     getProducts
+     );
+     console.log(data)
+
+    const getTotalItems = () => null;
+
+    const handleAddToCart = (clickItem: CartItemType) => null;
+
+    const handleRemoveFromCart= () => null;
+
+    if (isLoading) return <LinearProgress />;
+    if (error) return <div>Something went wrong...</div>
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Wrapper>
+    <Grid container spacing ={3}>
+      {data?.map(item =>(
+        <Grid item key={item.id} xs={12} sm={4}>
+          <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+      ))}
+      </Grid>
+   </Wrapper>
   );
 }
 
 export default App;
+
---
+++ src/item/item.tsx
@@ -0,0 +1,26 @@
+import Button from '@mui/material/Button' 
+import { type } from '@testing-library/user-event/dist/type'
+// Types
+import { CartItemType } from '../App'
+// Styles
+import { Wrapper } from '../App.styles'
+
+type Props = {
+    item: CartItemType;
+    handleAddToCart: (clickedItem: CartItemType ) => void;
+
+}
+
+const Item: React.FC<Props> = ({item, handleAddToCart }) => (
+    <Wrapper>
+        <img src={item.image} alt={item.title} />
+        <div>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <h3>${item.price}</h3>
+        </div>
+        <Button onClick={() => handleAddToCart(item)}> Add to cart</Button>
+    </Wrapper>
+);
+
+export default Item;
